861ff5e2f8c29831bfbfbe14cac25d68781a2b7e
Caleb Sigmon <cjsigmon@email.unc.edu>
2024-04-04 03:56:19 -0400

    add zoomer for states

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 let selectedState;
 let selectedStateElement;
 let slider = document.getElementById("myRange");
-let topPercent = 50;
+let topPercent = 100;
 let topPercentElement = document.getElementById("topPercent");
 let countyCountElement = document.getElementById("countyCount");
-let countyCount = data.length/2;
+let countyCount = data.length;
 countyCountElement.innerText = countyCount;
 slider.oninput = function() {
   topPercent = 100 - this.value;
@@ -43,11 +43,15 @@ function reRender() {
 }
 
 function renderStateChart() {
-  const stateData = data.filter((c) => {
+  let stateData = data.filter((c) => {
     return c.State === selectedState;
   }).sort((a,b) => {
     return (a[selectAttr.value] / a[denom]) - (b[selectAttr.value] / b[denom]);
   });
+  countyCount = Math.floor(stateData.length * (topPercent / 100));
+  countyCountElement.innerText = countyCount;
+  stateData = stateData.slice(-countyCount);
+
   let keys = stateData.map((c) => {return c.County});
   let vals = stateData.map((c) => {return c[selectAttr.value] / c[denom] * 100});
   let backgroundColor = stateData.map((c) => {
